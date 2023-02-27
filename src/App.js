481ffb967a85928f4a63ad9ba12d9b0991d090e8
Sol-Mayers <rinotello@yandex.ru>
2023-02-27 18:09:39 +0300

    some style changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
 
     return (
         <div className="App">
-            <h1 style={{ fontSize: 70, color: "red" }}>value = {state}</h1>
+            <h1
+                style={{
+                    fontSize: 70,
+                    color: "green",
+                    textDecoration: "underline",
+                }}
+            >
+                value = {state}
+            </h1>
             <button
                 style={{ background: "yellow", color: "black", padding: 15 }}
                 onClick={increment}
